2bc00b7d51b10335f7d838bfb41a77fbd4aad207
Vinicius B <vinicius_bezerra@hotmail.com>
2020-04-28 07:37:18 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -70,6 +70,11 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null,"Jardinagem");
+		Categoria cat5 = new Categoria(null,"Brinquedos");
+		Categoria cat6 = new Categoria(null,"Eletroportateis");
+		Categoria cat7 = new Categoria(null,"Papelaria");
 		
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
@@ -82,7 +87,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		repo.saveAll(Arrays.asList(cat1,cat2));
+		repo.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		prodRep.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null,"Minas Gerais");
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,23 +1,23 @@
 package com.nelioalves.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.security.Provider.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
-import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 
 
@@ -66,5 +66,18 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+
+	@RequestMapping( value = "/page" , method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		@RequestParam(value = "page", defaultValue = "0") Integer page, 
+		@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+		@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+		@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Categoria> list = serv.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+
 		
 }
